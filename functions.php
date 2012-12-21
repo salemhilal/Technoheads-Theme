6c65b01d740ab2df3a54a8e98535a5023b049ae0
Salem <salemhilal@gmail.com>
2012-12-21 03:02:22 -0500

    Updated functions.php, added some .js scripts.

--- functions.php
+++ functions.php
@@ -30,3 +30,19 @@ if (!function_exists('technoheads_setup' )):
   }
 endif;
 add_action('after_setup_theme', 'technoheads_setup');
+
+// Enqueue scripts and styles
+function shape_scripts(){
+  wp_enqueue_style('style', get_stylesheet_uri());
+
+  if(is_singular() && comments_open() && get_option('thread_comments')){
+    wp_enqueue_script('comment-reply');
+  }
+
+  wp_enqueue_script('small-menu', get_template_directory_uri() . '/js/small-menu.js', array('jquery'), '20120206', true);
+
+  if(is_singular() && wp_attachment_is_image()){
+    wp_enqueue_script('keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array('jquery'), '20120202');
+  }
+}
+add_action('wp_enqueue_scripts', 'shape_scripts');
